ebb23ab3080d646ae10e0d0744dae0be3dcd9aa5
Roberto Zagni <roberto.zagni@kaito.fi>
2020-10-14 11:20:32 +0000

    added sources with documentation and freshness

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -8,7 +8,7 @@ with payments as (
         AMOUNT / 100 as AMOUNT,     -- amount is in cents, we want it in dollars
         CREATED as created_date,
         _BATCHED_AT as batched_ts
-    from raw.STRIPE.PAYMENT
+    from {{ source('stripe','payment') }}
 )
 
 select * from payments
